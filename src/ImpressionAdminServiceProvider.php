e6d19ade29980445ab1085a1e1a9b25c95fec229
Oleh Talanoff <talanov.o@gmail.com>
2018-11-04 18:15:23 +0200

    Add resources

--- src/ImpressionAdminServiceProvider.php
+++ src/ImpressionAdminServiceProvider.php
@@ -14,7 +14,9 @@ class ImpressionAdminServiceProvider extends \Illuminate\Support\ServiceProvider
 		$this->loadViewsFrom(__DIR__.'/views', 'impression-admin');
 
     $this->publishes([
-        __DIR__.'/views' => resource_path('views/vendor/admin'),
+        __DIR__.'/views' => resource_path('views/admin'),
+        __DIR__.'/js' => resource_path('js'),
+        __DIR__.'/sass' => resource_path('sass'),
     ]);
 	}
 }
