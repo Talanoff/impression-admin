88e72fdded724f49e30c53332d91435844e94afa
Oleh Talanoff <talanov.o@gmail.com>
2018-12-18 11:18:20 +0200

    Config added. Update models.

--- src/Elements/NavigationElement.php
+++ src/Elements/NavigationElement.php
@@ -9,13 +9,15 @@ class NavigationElement
 	public $compare;
 	public $icon;
 	public $unread;
+	public $submenu;
 
-	public function __construct($name, $route, $icon, $compare = '*', $unread = null)
+	public function __construct($name, $route, $icon = 'i-settings', $compare = '*', $unread = null, $submenu = [])
 	{
 		$this->name = $name;
 		$this->route = config('impression-admin.route') . ".{$route}.index";
 		$this->icon = $icon;
 		$this->compare = $compare ? config('impression-admin.route') . ".{$route}.{$compare}" : null;
 		$this->unread = $unread;
+		$this->submenu = (object) $submenu;
 	}
 }
--- src/configs/impression-admin.php
+++ src/configs/impression-admin.php
@@ -3,4 +3,7 @@
 return [
 	'translatable_class' => 'Talanoff\\ImpressionAdmin\\Models\\Translate',
 	'mediable_class' => 'Talanoff\\ImpressionAdmin\\Models\\Media',
+
+	/* Admin route prefix */
+	'route' => 'admin',
 ];
