5320bbd6d76ccec1ef0b6a8c99640774f5ce7c3a
Oleh Talanoff <talanov.o@gmail.com>
2019-01-02 11:31:00 +0200

    Update navigation items

--- src/Elements/NavigationElement.php
+++ src/Elements/NavigationElement.php
@@ -24,13 +24,17 @@ class NavigationElement
 		$this->route = config('impression-admin.route') . ".{$args->route}.index";
 		$this->unread = $args->unread ?? null;
 
-		if (is_array($args->compare)) {
-			$current = explode('.', app('router')->currentRouteName());
-			$this->compare = collect($current)->contains(function($r) use ($args) {
-				return in_array($r, $args->compare);
-			});
+		if (isset($args->compare)) {
+			if (is_array($args->compare)) {
+				$current = explode('.', app('router')->currentRouteName());
+				$this->compare = collect($current)->contains(function($r) use ($args) {
+					return in_array($r, $args->compare);
+				});
+			} else {
+				$this->compare = app('router')->currentRouteNamed(config('impression-admin.route') . ".{$args->route}.{$args->compare}");
+			}
 		} else {
-			$this->compare = app('router')->currentRouteNamed(config('impression-admin.route') . ".{$args->route}.{$args->compare}");
+			$this->compare = app('router')->currentRouteNamed(config('impression-admin.route') . ".{$args->route}.*");
 		}
 
 		$this->submenu = (object) $args->submenu;
