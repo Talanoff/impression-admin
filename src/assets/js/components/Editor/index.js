aa7c5054680398243d2f86a7448074b2ebc605aa
Oleh Talanoff <talanov.o@gmail.com>
2019-01-10 10:36:17 +0200

    Add multi images uploader

--- src/assets/js/components/Editor/index.js
+++ src/assets/js/components/Editor/index.js
@@ -1,7 +1,9 @@
 import Editor from './Wysiwyg';
 import ImageUploader from './SingleImageUploader';
+import MultiUploader from './MultiImageUploader';
 
 export default {
   Editor,
   ImageUploader,
+  MultiUploader
 }
